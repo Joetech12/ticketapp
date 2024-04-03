860783bd0959749e6033b48832a9ad4bf38a70bb
Joetech12 <ifeanyi4umeh@gmail.com>
2024-04-03 12:56:35 +0100

    initiated project to create ticket page

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import prisma from "@/prisma/db";
+import DataTable from "./DataTable";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 
-const TicketsPage = () => {
+const TicketsPage = async () => {
+  const tickets = await prisma.ticket.findMany();
+
+  // console.log({tickets})
+
   return (
     <div>
-      Ticket Page
+      <Link
+        href='/tickets/new'
+        className={buttonVariants({ variant: "default" })}
+      >
+        New Ticket
+      </Link>
+      <DataTable tickets={tickets} />
     </div>
-  )
-}
+  );
+};
 
-export default TicketsPage
+export default TicketsPage;
